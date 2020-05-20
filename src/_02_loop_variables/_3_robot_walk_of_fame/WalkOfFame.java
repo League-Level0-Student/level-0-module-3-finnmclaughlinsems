81f7dcf639d817e750466abaf6d560610ab6c166
root <root@90cb6cf4c887>
2020-05-20 23:28:58 +0000

    loop varibles

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,9 +9,19 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(50);
+		rob.penDown();
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		rob.move(100);
+		rob.turn(144);
+		rob.move(100);
+		rob.turn(144);
+		rob.move(100);
+		rob.turn(144);
+		rob.move(100);
+		rob.turn(144);
+		rob.move(100);
+		rob.turn(144);
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
